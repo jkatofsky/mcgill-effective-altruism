5be9d694efe0ecc1226da6bd39bf677f1503b653
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2020-12-29 13:30:47 -0500

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => (
+    <p>Sorry, the page you are looking for could not be found. <Link to="/">Go home</Link>.</p>
+);
