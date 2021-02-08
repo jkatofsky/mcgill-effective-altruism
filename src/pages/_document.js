81280375f99dfdebcda1760de20346cd80d1e6c8
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-02-08 17:27:46 -0500

    tweak some exec bios, fix meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,13 +17,13 @@ class MyDocument extends Document {
                     <meta name="keywords" content="Effective Altruism, EA, Effective Giving, McGill" />
 
                     <meta name="description" content="Doing good better. The McGill chapter of the effective altruism movement." />
-                    <meta name="twitter:card" value="Doing good better. The McGill chapter of the effective altruism movement." />
-                    <meta property="og:description" content="Doing good better. The McGill chapter of the effective altruism movement." />
 
-                    {/* TODO */}
-                    <meta property="og:url" content="" />
-                    <meta property="og:image" content="" />
-                    <meta property="twitter:image" content="" />
+                    <meta property="og:description" content="Doing good better. The McGill chapter of the effective altruism movement." />
+                    <meta property="og:url" content="https://effective-altruism-mcgill.org" />
+                    <meta property="og:image" content="/images/ea-mcgill-logo.png" />
+
+                    <meta name="twitter:card" value="Doing good better. The McGill chapter of the effective altruism movement." />
+                    <meta property="twitter:image" content="/images/ea-mcgill-logo.png" />
                 </Head>
                 <body>
                     <Main />
--- src/pages/team.js
+++ src/pages/team.js
@@ -10,7 +10,7 @@ const execs = [
     {
         name: "Anna Mouland",
         position: "Vice President",
-        bio: "Anna is a 3rd year honours philosophy student with minors in economics and computer science. Anna found out about EA through an AI philosophy class, and resuscitated the McGill branch in January of 2020. Anna has taken the OFTW pledge and ran two fellowships to date, she also does contract work for Ought. She hopes the McGill branch will become an EA hub in Canada!",
+        bio: "Anna is a 3rd year Honours Philosophy student with minors in Economics and Computer Science. Anna found out about EA through an AI philosophy class, and resuscitated the McGill branch in January of 2020. Anna has taken the OFTW pledge and ran two fellowships to date, she also does contract work for Ought. She hopes the McGill branch will become an EA hub in Canada!",
         image: `${picsDir}/Anna.jpg`
     },
     {
@@ -22,13 +22,13 @@ const execs = [
     {
         name: "Annie Klar",
         position: "VP Events",
-        bio: "Annie is currently completing her Bachelor of Arts with a major in Political Science at McGill in Montreal. She hopes to use her degree and her minors in international development and english literature to help contribute to achieving sustainable development world wide. She is attracted to effective altruism due to its tactical approach to achieving good, and is excited to grow as a both a student and a member of the global community.",
+        bio: "Annie is currently completing her Bachelor of Arts with a major in Political Science at McGill. She hopes to use her degree and her minors in international development and english literature to help contribute to achieving sustainable development world wide. She is attracted to effective altruism due to its tactical approach to achieving good, and is excited to grow as a both a student and a member of the global community.",
         image: `${picsDir}/Annie.jpg`
     },
     {
         name: "Sarah Cleveland",
         position: "VP Engagement",
-        bio: "Sarah was first introduced to Effective Altruism through the Yale EA Fellowship in the summer of 2020. Since starting her own non profit, Hopewell Gives Back, in her hometown, she has wanted to find ways to further her impact on a larger scale. At McGill she studies economics and molecular biology, aiming to bring EA values into global health policy development.",
+        bio: "Sarah is studyingSarah was first introduced to Effective Altruism through the Yale EA Fellowship in the summer of 2020. Since starting her own non profit, Hopewell Gives Back, in her hometown, she has wanted to find ways to further her impact on a larger scale. At McGill she studies economics and molecular biology, aiming to bring EA values into global health policy development.",
         image: `${picsDir}/Sarah.jpg`
     },
     {
@@ -40,13 +40,13 @@ const execs = [
     {
         name: "Josh Katofsky",
         position: "VP Logistics",
-        bio: "Josh is a third-year McGill student majoring in Computer Science with a dual minor in Political Science and Philosophy. He is particularly interested in AI ethics and sustainability, and aims to pursue a career in software for social good.",
+        bio: "Josh is a 3rd year Computer Science student with a dual minor in Political Science and Philosophy. He is particularly interested in AI ethics and sustainability, and aims to pursue a career in software for social good.",
         image: `${picsDir}/Josh.png`
     },
     {
         name: "Helena Lang",
         position: "VP Affairs",
-        bio: "",
+        bio: "Helena is an Honours Philosophy student at McGill with minors in Economics and Political Science. She first stumbled upon EA in high school and has continued to fall into deeper rabbit holes since, especially on AI ethics and philosophy of science. Her interests include all things philosophy, political economy (and critiques thereof), and creative writing. At the moment it looks like she will be applying to grad school in philosophy.",
         image: ""
     },
     {
