8d7d877f2e6395fbf9ccaa63a0538dd8581e97e6
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-02-22 00:38:56 -0500

    remove extra period

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
         <hr />
         <p>© 2021 Effective Altruists at McGill</p>
         <p>Created by <a href="https://www.linkedin.com/in/josh-katofsky/" target="_blank">Josh Katofsky</a></p>
-        <p>This website is <a href="https://github.com/jkatofsky/mcgill-effective-altruism" target="_blank">open source</a></p>.
-            </footer >
+        <p>This website is <a href="https://github.com/jkatofsky/mcgill-effective-altruism" target="_blank">open source</a></p>
+    </footer >
 }
 
 export default Footer;
